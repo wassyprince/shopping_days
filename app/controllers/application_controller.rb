badeec9f6c329ba5fb5c2043efa7fb3657eddc6a
wassyprince <wa3no1978_reloaded@docomo.ne.jp>
2025-11-20 15:07:22 +0900

    render設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
   #before_action :authenticate_user! #この記述により、ログインしていないユーザーをログインページの画面に促すことができます。
   before_action :configure_permitted_parameters, if: :devise_controller? # :devise_controller?というdeviseのヘルパーメソッド名を指定して、#もしdeviseに関するコントローラーの処理であれば、そのときだけconfigure_permitted_parametersメソッドを実行するように設定しています。
-
+  
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path   # ← ログアウト後にサインインページへ
+  end
+  
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
