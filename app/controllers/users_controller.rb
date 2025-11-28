a06c7169a550740df0731b41730acb0d9b074dd2
wassyprince <wa3no1978_reloaded@docomo.ne.jp>
2025-11-28 14:47:07 +0900

    ユーザー編集・削除機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,34 @@
 class UsersController < ApplicationController
-  #before_action :authenticate_user! #この記述により、ログインしていないユーザーをログインページの画面に促すことができます。
+  before_action :set_user, only: [:edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def edit
-    @user = current_user
   end
 
   def update
-    @user = current_user
     if @user.update(user_params)
       redirect_to root_path  # 更新成功
     else
-      render :edit, status: :unprocessable_entity  # 更新失敗時は再度編集フォームを表示
+      render :edit
     end
+  end
+
+  def destroy
+    redirect_to root_path, notice: "アカウントを削除しました"
   end
 
   private
 
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def correct_user
+    redirect_to root_path, alert: "権限がありません" unless @user == current_user
+  end
+
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
