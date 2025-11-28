148764d671bf45dcb23935bede28366e93ecb245
wassyprince <wa3no1978_reloaded@docomo.ne.jp>
2025-11-28 16:08:09 +0900

    カレンダーのタイトルの月を変える

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,12 +4,20 @@ class EventsController < ApplicationController
   def calendar
     # 日付ごとの買い物リストの数を取得
     @shopping_counts = ShoppingList.group(:date).count
-    @date = params[:month]&.to_date || Date.today
-
+    
     # カレンダーに表示するために、日付ごとに買い物リストを設定
     @shopping_list = ShoppingList.find_by(date: @date, user: current_user)  # 例: 現在のユーザーの買い物リストを取得
     
     # 予定を取得して @plans に入れる
-    @plans = Plan.all
+    @plans = Plan.all   
+    
+    @date =
+      if params[:start_date].present? # カレンダーのタイトル更新
+        Date.parse(params[:start_date])
+      elsif params[:month].present? 
+        params[:month].to_date
+      else
+        Date.today
+      end
   end
 end
