60bcc2e3473d43a18daf21d3adca6d00944dca8a
wassyprince <wa3no1978_reloaded@docomo.ne.jp>
2025-12-03 15:06:17 +0900

    履歴微修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :shopping_list
-  has_many   :edit_histories, dependent: :destroy
+  has_many   :edit_histories, dependent: :nullify
 
   validates :name, presence: true  # アイテム名が必須
   validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 1 }  # 数量が1以上
