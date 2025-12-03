4db9c4fb13e9b574bd519c280979d28e37619326
wassyprince <wa3no1978_reloaded@docomo.ne.jp>
2025-12-03 15:32:16 +0900

    BASIC認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,25 @@
 class ApplicationController < ActionController::Base
-  #before_action :authenticate_user! #この記述により、ログインしていないユーザーをログインページの画面に促すことができます。
-  before_action :configure_permitted_parameters, if: :devise_controller? # :devise_controller?というdeviseのヘルパーメソッド名を指定して、#もしdeviseに関するコントローラーの処理であれば、そのときだけconfigure_permitted_parametersメソッドを実行するように設定しています。
+  before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller? 
   
   def after_sign_in_path_for(resource)
-    calendar_path # ここでカレンダーのルートを指定
+    calendar_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
-    new_user_session_path   # ← ログアウト後にサインインページへ
+    new_user_session_path
   end
   
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+ 
+  def basic_auth
+    if Rails.env.production?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      end
+    end
   end
 end
