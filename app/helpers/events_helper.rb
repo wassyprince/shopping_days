123c4f0f4b2872ab09d476721840f9870fd11473
wassyprince <wa3no1978_reloaded@docomo.ne.jp>
2026-02-16 10:26:16 +0900

    Update events_helper.rb

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,17 +1,16 @@
 module EventsHelper 
   ROKUYOU = %w[先勝 友引 先負 仏滅 大安 赤口].freeze 
-
   def rokuyou(date) 
-    index = (date.month + date.day) % 6 
-    text = ROKUYOU[index]
-
-    styled = 
-    if text == "大安" 
-      "<span style='color:red;'>#{text}</span>"
-    else 
-      "<span style='color:black;'>#{text}</span>"
-    end 
+    return "" if date.nil? 
     
-    styled.html_safe
-  end 
+    date = date.to_date 
+    
+    base = Date.new(1900, 1, 1) # 六曜の基準日（大安） 
+    
+    diff = (date - base).to_i 
+    text = ROKUYOU[diff % 6] 
+
+    color = text == "大安" ? "red" : "black" 
+    "<span style='color:#{color};'>#{text}</span>".html_safe 
+  end
 end
