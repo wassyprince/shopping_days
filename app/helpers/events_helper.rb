045c056451deb07510008ef87b7d853453c462b0
wassyprince <wa3no1978_reloaded@docomo.ne.jp>
2026-02-16 10:30:33 +0900

    六曜撤退

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,16 +1,2 @@
-module EventsHelper 
-  ROKUYOU = %w[先勝 友引 先負 仏滅 大安 赤口].freeze 
-  def rokuyou(date) 
-    return "" if date.nil? 
-    
-    date = date.to_date 
-    
-    base = Date.new(1900, 1, 1) # 六曜の基準日（大安） 
-    
-    diff = (date - base).to_i 
-    text = ROKUYOU[diff % 6] 
-
-    color = text == "大安" ? "red" : "black" 
-    "<span style='color:#{color};'>#{text}</span>".html_safe 
-  end
+module EventsHelper
 end
